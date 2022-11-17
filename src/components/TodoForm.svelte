3e5c272be351fff0ea4942637163a1a9798e1302
Zaki Samson <zakisamson@gmail.com>
2022-11-17 17:28:21 +0700

    added todo form, and todo stores

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,1 +1,26 @@
-hey
+<script>
+    import {addTodo} from "../stores/todoStore.js"
+
+    let todo = ""
+
+    const handleSubmit = () => {
+        addTodo(todo)
+        todo = ""
+        console.log("submit")
+    }
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+	<div class="flex flex-col text-sm mb-2">
+		<label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+		<input
+            bind:value={todo}
+			class="appereance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			type="text"
+			name="todo"
+			id=""
+			placeholder="Whatcu gotta do?"
+		/>
+	</div>
+    <button class="w-dull shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4" type="submit">Submit</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,5 @@
     <h1 class="text-2xl font-bold text-center text-gray md:text-3xl">
         My Todo List
     </h1>
+    <TodoForm />
 </main>
