821905b55b740aa37dd20fd4d0eecd0aae185dbb
Zaki Samson <zakisamson@gmail.com>
2022-11-17 17:39:15 +0700

    added todo list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script>
     import TodoForm from "../components/TodoForm.svelte";
+    import Todo from "../components/Todo.svelte"
+    import {todos} from "../stores/todoStore"
 </script>
 <main>
     <h1 class="text-2xl font-bold text-center text-gray md:text-3xl">
         My Todo List
     </h1>
     <TodoForm />
+    {#each $todos as todo}
+        <Todo todo={todo} index={todo.id} />
+    {/each}
 </main>
